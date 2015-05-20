8a49b14a01208a6b7d2e6af2ea2a3b31b1e0cf7f
Chinmay Garde <chinmaygarde@google.com>
2015-05-20 15:24:25 -0700

    Add iOS Sky shell

---
+++ sky/shell/ios/sky_view_controller.m
@@ -0,0 +1,16 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "sky_view_controller.h"
+
+@implementation SkyViewController
+
+-(void) viewDidLoad {
+  self.view.backgroundColor = [UIColor colorWithRed: 0.494
+                                              green: 0.752
+                                               blue: 0.933
+                                              alpha: 1.000];
+}
+
+@end
