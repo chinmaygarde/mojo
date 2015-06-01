a076f12ff161e44a198ecf6fb1794f7aba41801f
Chinmay Garde <chinmaygarde@google.com>
2015-06-01 15:10:17 -0700

    Initial implementation of the sky/shell on iOS

---
+++ sky/shell/ios/main_ios.mm
@@ -0,0 +1,56 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+#include <asl.h>
+#import "sky_app_delegate.h"
+#include "base/at_exit.h"
+#include "base/logging.h"
+#include "base/i18n/icu_util.h"
+#include "base/command_line.h"
+#include "base/mac/scoped_nsautorelease_pool.h"
+#include "ui/gl/gl_surface.h"
+
+static void InitializeLogging() {
+  logging::LoggingSettings settings;
+  settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
+  logging::InitLogging(settings);
+  logging::SetLogItems(false,   // Process ID
+                       false,   // Thread ID
+                       false,   // Timestamp
+                       false);  // Tick count
+}
+
+static void RedirectIOConnectionsToSyslog() {
+  asl_log_descriptor(NULL, NULL, ASL_LEVEL_INFO, STDOUT_FILENO,
+                     ASL_LOG_DESCRIPTOR_WRITE);
+  asl_log_descriptor(NULL, NULL, ASL_LEVEL_NOTICE, STDERR_FILENO,
+                     ASL_LOG_DESCRIPTOR_WRITE);
+}
+
+#ifndef NDEBUG
+
+static void debugger_hook_main(void) {
+  // By default, LLDB breaks way too early. This is before libraries have been
+  // loaded and __attribute__((constructor)) methods have been called. In most
+  // situations, this is unnecessary. Also, breakpoint resolution is not
+  // immediate. So we provide this hook to break on.
+}
+
+#endif
+
+int main(int argc, char * argv[]) {
+  base::mac::ScopedNSAutoreleasePool pool;
+  base::AtExitManager exit_manager;
+#ifndef NDEBUG
+  debugger_hook_main();
+#endif
+  RedirectIOConnectionsToSyslog();
+  base::CommandLine::Init(0, nullptr);
+  InitializeLogging();
+  base::i18n::InitializeICU();
+  gfx::GLSurface::InitializeOneOff();
+  return UIApplicationMain(argc, argv, nil,
+                           NSStringFromClass([SkyAppDelegate class]));
+}
